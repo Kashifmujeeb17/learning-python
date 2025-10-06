3366f8dc7584512dd56e180cedd4b8d95ccec86e
Kashifmujeeb17 <kashif.mujeeb911@gmail.com>
2025-10-06 21:01:19 +0500

    update

--- day-2.py
+++ day-2.py
@@ -1,3 +1,8 @@
-gold_coin = input("Please Enter the gold coin: any positive integer: ")
-gold_coin_2 = input("Please Enter the gold coin 2: any positive integer: ")
+gold_coin = int(input("Please Enter the gold coin: any positive integer: "))
+gold_coin_2 = int(input("Please Enter the gold coin 2: any positive integer: "))
+
+
+total = gold_coin + gold_coin_2
+print("The gold coin is: ", total)
+
 
